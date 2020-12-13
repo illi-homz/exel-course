4599020e07dd71ffa8309a5995ab12b02a48fd61
illi-homz <gopkabel@mail.ru>
2020-12-14 02:12:46 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
