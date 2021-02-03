735addcfc22e6fe8dc93d7507e4c0fac6a72b1ff
illi <illi@illis-Mac-mini.local>
2021-02-03 16:07:50 +0300

    change dom.js

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,18 @@
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error('no $root provided for DomListener')
     }
+
     this.$root = $root
+    this.listeners = listeners
   }
+
+  initDOMListeners() {
+  	this.listeners.forEach(listener => {
+  		// console.log(this.$root)
+  	})
+  }
+
+  removeDOMListeners() {}
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -2,7 +2,16 @@ import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
   // возвращает шаблон компонента
+
+  constructor($root, options = {}) {
+  	super($root, options.listeners)
+  }
+
   toHTML() {
     return ''
+  }
+
+  init() {
+  	this.initDOMListeners()
   }
 }
